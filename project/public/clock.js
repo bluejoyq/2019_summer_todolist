6a25f56a5df1812100231d49ac4bf5df5eae3901
bluejoyq <bluejoyq@naver.com>
2019-08-28 22:24:59 +0900

    2019-08-28 layout making

--- project/public/clock.js
+++ project/public/clock.js
@@ -3,11 +3,11 @@ function clock(){
     const hour = date.getHours();
     const minute = date.getMinutes();
     const second = date.getSeconds();
-    let now_time= hour > 10 ? hour : "0" + hour;
+    let now_time= hour >= 10 ? hour : "0" + hour;
     now_time+= ":"
-    now_time+= minute > 10 ? minute : "0" + minute;
+    now_time+= minute >= 10 ? minute : "0" + minute;
     now_time+= ":"
-    now_time+= second > 10 ? second : "0" + second;
+    now_time+= second >= 10 ? second : "0" + second;
     $(".clock").text(now_time);
 }
 
