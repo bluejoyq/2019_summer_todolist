106bf53c81cc8e53c04388a12c0a7d8e6d9eca42
bluejoyq <bluejoyq@naver.com>
2019-09-01 04:52:44 +0900

    2019-09-01 layout update

--- project/app.js
+++ project/app.js
@@ -1,11 +1,20 @@
-const moment = require('moment');
 const express = require('express');
 const app = express();
+
+let schedule = new Array();
 
 app.use(express.static(__dirname+"/public"));
 
 app.get('/',(req,res)=>{
-    res.sendFile('index.html');
+    res.sendFile('/index.html');
+});
+
+app.post('/add',(req,res)=>{
+    res.send("asdsa");
+    // 밑에 추가하는 창 열리게
+    // 날짜 설명 알람여부등 추가후
+    // submit
+    // schedule 어레이에 추가후 일정 목록 갱신
 });
 
 app.listen(3000,()=>{
--- project/public/clock.js
+++ project/public/clock.js
@@ -1,17 +1,33 @@
+const date = new Date();
+const week = ["일","월","화","수","목","금","토"];
+
 function clock(){
-    const date = new Date();
     const hour = date.getHours();
     const minute = date.getMinutes();
     const second = date.getSeconds();
+
     let now_time= hour >= 10 ? hour : "0" + hour;
     now_time+= ":"
     now_time+= minute >= 10 ? minute : "0" + minute;
     now_time+= ":"
     now_time+= second >= 10 ? second : "0" + second;
+
+    if(hour == 0 && minute == 0 && second ==0) 
+        day_count(date);
     $(".clock").text(now_time);
+}
+function day_count(date){
+    const day = date.getDate();
+    const month = date.getMonth();
+    const years = date.getFullYear();
+    const days = date.getDay();
+
+    let now_day = years + "/" + (month+1) + "/" + day + " " + week[days];
+    $(".date").text(now_day);
 }
 
 $(document).ready(function(){
-	clock(); 
+    clock(); 
+    day_count(date);
     setInterval(clock,1000);
 });
