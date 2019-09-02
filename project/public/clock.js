52f249227bb37847570e48df5ed761e853fc4e31
bluejoyq <bluejoyq@naver.com>
2019-09-02 12:58:06 +0900

    2019-09-02 contemplating

--- project/public/clock.js
+++ project/public/clock.js
@@ -2,20 +2,22 @@ const date = new Date();
 const week = ["일","월","화","수","목","금","토"];
 
 function clock(){
+    const date = new Date();
     const hour = date.getHours();
     const minute = date.getMinutes();
     const second = date.getSeconds();
 
     let now_time= hour >= 10 ? hour : "0" + hour;
-    now_time+= ":"
+    now_time+= ":";
     now_time+= minute >= 10 ? minute : "0" + minute;
-    now_time+= ":"
+    now_time+= ":";
     now_time+= second >= 10 ? second : "0" + second;
 
     if(hour == 0 && minute == 0 && second ==0) 
         day_count(date);
-    $(".clock").text(now_time);
+    $("#clock").text(now_time);
 }
+
 function day_count(date){
     const day = date.getDate();
     const month = date.getMonth();
@@ -23,7 +25,7 @@ function day_count(date){
     const days = date.getDay();
 
     let now_day = years + "/" + (month+1) + "/" + day + " " + week[days];
-    $(".date").text(now_day);
+    $("#date").text(now_day);
 }
 
 $(document).ready(function(){
@@ -31,3 +33,11 @@ $(document).ready(function(){
     day_count(date);
     setInterval(clock,1000);
 });
+
+$("#btn_add").click(()=>{
+    $(".box-2").show();
+}); 
+
+$("#btn_hide").click(()=>{
+    $(".box-2").hide();
+}); 
